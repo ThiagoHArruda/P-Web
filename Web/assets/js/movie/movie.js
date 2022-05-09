78c91c8353672a8b1f5ffcaa7f87af2b03bb9f44
ThiagoHArruda <86895692+ThiagoHArruda@users.noreply.github.com>
2022-05-08 21:23:28 -0300

    Delete Web directory

--- Web/assets/js/movie/movie.js
+++
@@ -1,7 +0,0 @@
-import { genrerMovie } from './genderMovie.js'
-
-let carrocel = document.querySelectorAll('.catalogo__carrocel-lista')
-    //console.log(carrocel);
-
-let genrer = new genrerMovie()
-genrer.GenrerList
--- Web/assets/js/movie/movieView.js
+++
@@ -1,7 +0,0 @@
-export class movieView {
-    static template(element) {
-        return `<div class="carrocel__filme" style= "background-image: url(https://image.tmdb.org/t/p/original${element.Poster});"></div>
-      `
-    }
-
-}
